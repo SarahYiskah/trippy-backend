b16004b08add826389143d2e1dedc23bc1d6a427
github username <github email address>
2018-04-16 10:22:11 -0400

    profile loads

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::UsersController < ApplicationController
 
-  # before_action :authorize!, only: [:users_snacks]
+  # before_action :authorize!, only: [:users_following]
   skip_before_action :authenticate!, only: [:create]
 
   def create
@@ -29,18 +29,31 @@ class Api::V1::UsersController < ApplicationController
     end
   end
 
-  # def users_snacks
-  #   unless authorized?
-  #     render json: { take_a_hike: true}
-  #   else
-  #     @user = User.find_by(id: params[:user_id])
-  #     if @user
-  #       render json: @user.snacks
-  #     else
-  #       render json: true, :status => :not_found
-  #     end
-  #   end
-  # end
+  def users_followers
+    unless authorized?
+      render json: { take_a_hike: true}
+    else
+      @user = User.find_by(id: params[:user_id])
+      if @user
+        render json: @user.followers
+      else
+        render json: true, :status => :not_found
+      end
+    end
+  end
+
+  def users_following
+    unless authorized?
+      render json: { take_a_hike: true}
+    else
+      @user = User.find_by(id: params[:user_id])
+      if @user
+        render json: @user.following
+      else
+        render json: true, :status => :not_found
+      end
+    end
+  end
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # get '/snacks/', to: 'snacks#index'
-  # get '/users/:user_id/snacks', to: 'users#users_snacks''
+  # get '/users/:user_id/snacks', to: 'users#users_snacks'
   namespace :api do
    namespace :v1 do
      post '/signup', to: 'users#create'
      get '/users/:user_id', to: 'users#show'
+     get '/users/:user_id/followers', to: 'users#users_followers'
+     get '/users/:user_id/following', to: 'users#users_following'
      post '/sessions', to: 'sessions#create'
    end
  end
